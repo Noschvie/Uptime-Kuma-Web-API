0246655a90aa245db75aa875dc298ad15d256868
021-projects <20326979+021-projects@users.noreply.github.com>
2025-03-13 20:26:38 +0200

    refactor(api): fix IDE warnings

--- app/schemas/api.py
+++ app/schemas/api.py
@@ -1,6 +1,10 @@
 from typing import Any
 from pydantic import BaseModel
 
+
 class API(BaseModel):
     token: str
     api: Any
+
+    def __getitem__(self, item):
+        return getattr(self, item)
